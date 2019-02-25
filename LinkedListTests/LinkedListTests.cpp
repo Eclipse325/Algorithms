b834693bdd72572c5e645d6254ce7adf2cc41bff
Dimokku <32371247+Dimokku@users.noreply.github.com>
2019-02-26 01:41:14 +0300

    Update LinkedListTests.cpp

--- LinkedListTests/LinkedListTests.cpp
+++ LinkedListTests/LinkedListTests.cpp
@@ -11,27 +11,44 @@ namespace LinkedListTests
 	{
 	public:
 		
-		TEST_METHOD(is_size_Node_empty) //Check for the return of the empty list size
+		TEST_METHOD(get_empty_list_size_int) //Check for the return of the empty list size
 		{
 			LinkedList<int> lst;
 			Assert::IsTrue(lst.get_size() == 0);
 		}
 
+		TEST_METHOD(get_list_size_int) //Check for the return of list size
+		{
+			LinkedList<int> lst;
+			lst.push_back(5);
+			lst.push_back(4);
+			lst.push_back(3);
+			Assert::IsTrue(lst.get_size() == 3);
+		}
 
-		TEST_METHOD(List_is_Empty) //Check the emptiness of the list (at the beginning the list is always empty)
+		TEST_METHOD(List_is_Empty_int) //Check the emptiness of the list (at the beginning the list is always empty)
 		{
 			LinkedList<int> lst;
 			Assert::IsTrue(lst.isEmpty());
 		}
 
-		TEST_METHOD(is_clear_Node_empty) //Check to clear the empty list
+		TEST_METHOD(List_is_not_Empty_int)
+		{
+			LinkedList<int> lst;
+			lst.push_back(5);
+			lst.push_back(4);
+			lst.push_back(3);
+			Assert::IsFalse(lst.isEmpty());
+		}
+
+		TEST_METHOD(clear_empty_list_int) //Check to clear the empty list
 		{
 			LinkedList<int> lst;
 			lst.clear();
 			Assert::IsTrue(lst.isEmpty());
 		}
 
-		TEST_METHOD(is_clear_Node) //Check to clear the list
+		TEST_METHOD(clear_list_int) //Check to clear the list
 		{
 			LinkedList<int> lst;
 			lst.push_back(5);
@@ -41,192 +58,177 @@ namespace LinkedListTests
 			Assert::IsTrue(lst.isEmpty());
 		}
 
-		TEST_METHOD(is_at_Node) //Check taking a list item value
+		TEST_METHOD(at_list_item_int) //Check taking a list item value
 		{
 			LinkedList<int> lst;
 			lst.push_front(5);
 			Assert::IsTrue(lst.at(0) == 5);
 		}
 
-		TEST_METHOD(is_add_back_Node) //Check for adding item to the end
+		TEST_METHOD(at_out_of_range_int) //at out of range
 		{
 			LinkedList<int> lst;
-			lst.push_back(4);
-			lst.push_back(5);
-			lst.push_back(6);
-			Assert::IsTrue(lst.at(2) == 6);
-		}
-
-		TEST_METHOD(is_add_front_Node) //Check for adding item to the beginning
-		{
-			LinkedList<int> lst;
-			lst.push_front(4);
-			lst.push_front(5);
-			lst.push_front(6);
-			Assert::IsTrue(lst.at(0) == 6);
-		}
-
-		TEST_METHOD(is_insert_Node_empty) //Check to insert an item in an empty list
-		{
-			LinkedList<int> lst;
-			lst.insert(5, 1);
-			Assert::IsTrue(lst.at(0) == 1);
-		}
-
-		TEST_METHOD(is_insert_Node_one) //Check to insert an item by index into the list with one element
-		{
-			LinkedList<int> lst;
-			lst.push_back(5);
-			lst.insert(0, 6);
-			Assert::IsTrue(lst.at(0) == 6);
-		}
-
-		TEST_METHOD(is_insert_Node_over) //Check for insertion of an item into the list with an index outside the list
-										//(index is reduced to the maximum index of the list)
-		{
-			LinkedList<int> lst;
-			lst.push_back(5);
-			lst.insert(2, 6);
-			Assert::IsTrue(lst.at(0) == 6);
-		}
-
-		TEST_METHOD(is_insert_Node) //Check to insert an element into the list by index
-		{
-			LinkedList<int> lst;
-			lst.push_back(5);
-			lst.push_back(4);
-			lst.insert(6, 1);
-			Assert::IsTrue(lst.at(2) == 4);
-		}
-
-		TEST_METHOD(is_pop_front_Node_empty) //Check to remove the first item of the empty list
-		{
-			LinkedList<int> lst;
-			lst.pop_front();
-			Assert::IsTrue(lst.isEmpty());
-		}
-
-		TEST_METHOD(is_pop_front_Node_one) //Check to remove the first item of the list with one element
-		{
-			LinkedList<int> lst;
-			lst.push_front(5);
-			lst.pop_front();
-			Assert::IsTrue(lst.isEmpty());
-		}
-
-		TEST_METHOD(is_pop_front_Node) //Check to remove the first item of the list
-		{
-			LinkedList<int> lst;
-			lst.push_front(5);
-			lst.push_front(4);
-			lst.push_front(3);
-			lst.pop_front();
-			Assert::IsTrue((lst.at(0) == 4) && (lst.at(1) == 5) && (lst.get_size() == 2));
-		}
-
-		TEST_METHOD(is_pop_back_Node_null) //Check to remove the last item of the empty list
-		{
-			LinkedList<int> lst;
-			lst.pop_back();
-			Assert::IsTrue(lst.isEmpty());
-		}
-
-		TEST_METHOD(is_pop_back_Node_one) //Check to remove the last item in the list with one item
-		{
-			LinkedList<int> lst;
-			lst.push_back(5);
-			lst.pop_back();
-			Assert::IsTrue(lst.isEmpty());
-		}
-
-		TEST_METHOD(is_pop_back_Node) //Check to remove the last element of the empty list
-		{
-			LinkedList<int> lst;
-			lst.push_back(5);
-			lst.push_back(4);
-			lst.push_back(3);
-			lst.pop_back();
-			Assert::IsTrue((lst.at(0) == 5) && (lst.at(1) == 4) && (lst.get_size() == 2));
-		}
-
-		TEST_METHOD(is_set_Node_null) //Check for replacement of an empty list item
-		{
-			LinkedList<int> lst;
-			//char error;
-			try {
-				lst.set(0, 5);;
-			}
-			catch (const std::out_of_range& error) {
-				Assert::AreEqual("Index is greater than list size", error.what());
-			}
-		}
-
-
-		TEST_METHOD(is_set_Node) //Check for correct replacement of the item value
-		{
-			LinkedList<int> lst;
-			lst.push_front(5);
-			lst.set(0, 1);
-			Assert::IsTrue(lst.at(0) == 1);
-		}
-
-		TEST_METHOD(is_delete_Node) //Check for deletion of element 
-		{
-			LinkedList<int> lst;
-			lst.push_front(5);
-			lst.push_front(6);
-			lst.remove(1);
-			Assert::IsTrue(lst.at(0) == 6 && lst.get_size() == 1);
-		}
-
-		TEST_METHOD(is_swap_Node) //Check the correctness of the exchange of two values by index 
-		{
-			LinkedList<int> lst;
-			lst.push_front(5);
-			lst.push_front(6);
-			lst.swap(0, 1);
-			Assert::IsTrue(lst.at(0) == 5 && lst.at(1) == 6);
-		}
-
-		TEST_METHOD(at_exception) //at function exception
-		{
-			LinkedList<int> lst;
-			//char error;
 			lst.push_front(5);
 			lst.push_front(6);
 			try {
 				lst.at(5);
 			}
 			catch (const std::out_of_range& error) {
-				Assert::AreEqual("Индекс больше, чем наибольший индекс списка", error.what());
+				Assert::AreEqual("Index is out of range", error.what());
 			}
 		}
 
-		TEST_METHOD(remove_exception) //remove function exception
+		TEST_METHOD(push_back_empty_list_char) //Check for adding item to the end of the empty list
+		{
+			LinkedList<char> lst;
+			lst.push_back('a');
+			Assert::IsTrue(lst.at(0) == 'a');
+		}
+
+		TEST_METHOD(push_back_char) //Check for adding item to the end
+		{
+			LinkedList<char> lst;
+			lst.push_back('a');
+			lst.push_back('b');
+			lst.push_back('c');
+			Assert::IsTrue(lst.at(2) == 'c');
+		}
+
+		TEST_METHOD(push_front_empty_list_char) //Check for adding item to the beginning of the empty list
+		{
+			LinkedList<char> lst;
+			lst.push_front('a');
+			Assert::IsTrue(lst.at(0) == 'a');
+		}
+
+		TEST_METHOD(push_front_char) //Check for adding item to the beginning
+		{
+			LinkedList<char> lst;
+			lst.push_front('a');
+			lst.push_front('b');
+			lst.push_front('c');
+			Assert::IsTrue(lst.at(0) == 'c');
+		}
+
+		TEST_METHOD(insert_item_empty_list_double) //Check to insert an item in an empty list
+		{
+			LinkedList<double> lst;
+			lst.insert(5, 1.125);
+			Assert::IsTrue(lst.at(0) == 1.125);
+		}
+
+		TEST_METHOD(insert_item_first_double) //Check to insert an item by index into the beginning
+		{
+			LinkedList<double> lst;
+			lst.push_back(5.5);
+			lst.insert(0, 6.6);
+			Assert::IsTrue(lst.at(0) == 6.6);
+		}
+
+		TEST_METHOD(insert_item_last_double) //Check to insert an item by index into the end
+		{
+			LinkedList<double> lst;
+			lst.push_back(5.5);
+			lst.push_back(5.6);
+			lst.push_back(5.7);
+			lst.insert(2, 6.8);
+			Assert::IsTrue(lst.at(2) == 6.8);
+		}
+
+		TEST_METHOD(insert_double) //Check to insert an element into the list by index
+		{
+			LinkedList<double> lst;
+			lst.push_back(5.5);
+			lst.push_back(4.5);
+			lst.push_back(5.7);
+			lst.push_back(4.5);
+			lst.push_back(5.1);
+			lst.push_back(4.3);
+			lst.insert(3, 1.3);
+			Assert::IsTrue(lst.at(3) == 1.3);
+		}
+
+		TEST_METHOD(is_insert_over_double) //Check for insertion of an item into the list with an index outside the list
+										//(index is reduced to the maximum index of the list)
+		{
+			LinkedList<double> lst;
+			lst.push_back(5.5);
+			lst.push_back(5.5);
+			lst.push_back(5.5);
+			lst.insert(5, 6.1);
+			Assert::IsTrue(lst.at(2) == 6.1);
+		}
+
+		TEST_METHOD(pop_front_empty_list_char) //Check to remove the first item of the empty list
+		{
+			LinkedList<char> lst;
+			lst.pop_front();
+			Assert::IsTrue(lst.isEmpty());
+		}
+
+		TEST_METHOD(pop_front_char) //Check to remove the first item
+		{
+			LinkedList<char> lst;
+			lst.push_front('c');
+			lst.push_front('b');
+			lst.push_front('a');
+			lst.pop_front();
+			Assert::IsTrue((lst.at(0) == 'b') && (lst.at(1) == 'c') && (lst.get_size() == 2));
+		}
+
+		TEST_METHOD(pop_back_empty_list_char) //Check to remove the last item of the empty list
+		{
+			LinkedList<char> lst;
+			lst.pop_back();
+			Assert::IsTrue(lst.isEmpty());
+		}
+
+		TEST_METHOD(pop_back_char) //Check to remove the last element
+		{
+			LinkedList<char> lst;
+			lst.push_back('c');
+			lst.push_back('b');
+			lst.push_back('a');
+			lst.pop_back();
+			Assert::IsTrue((lst.at(0) == 'c') && (lst.at(1) == 'b') && (lst.get_size() == 2));
+		}
+
+		TEST_METHOD(set_over_of_range_int) //Check for replacement of an empty/over list item
 		{
 			LinkedList<int> lst;
-			//char error;
-			lst.push_front(5);
-			lst.push_front(6);
 			try {
-				lst.remove(5);
+				lst.set(0, 5);;
 			}
 			catch (const std::out_of_range& error) {
-				Assert::AreEqual("Index is greater than list size", error.what());
+				Assert::AreEqual("Index is out of range", error.what());
 			}
 		}
 
-		TEST_METHOD(set_exception) //Set function exception
+		TEST_METHOD(set_Node_char) //Check for correct replacement of the item value
+		{
+			LinkedList<char> lst;
+			lst.push_front('a');
+			lst.set(0, 'c');
+			Assert::IsTrue(lst.at(0) == 'c');
+		}
+
+		TEST_METHOD(remove_double) //Check for deletion of element 
+		{
+			LinkedList<double> lst;
+			lst.push_front(5.5);
+			lst.push_front(6.6);
+			lst.remove(1);
+			Assert::IsTrue(lst.at(0) == 6.6 && lst.get_size() == 1);
+		}
+
+		TEST_METHOD(remove_over_int) //Check for deletion of element out of range 
 		{
 			LinkedList<int> lst;
-			//char error;
-			lst.push_front(5);
-			lst.push_front(6);
 			try {
-				lst.set(5, 13);
+				lst.remove(1);
 			}
-			catch (const std::out_of_range &error) {
-				Assert::AreEqual("Index is greater than list size", error.what());
+			catch(const std::out_of_range& error){
+				Assert::AreEqual("Index is out of range", error.what());
 			}
 		}
 
