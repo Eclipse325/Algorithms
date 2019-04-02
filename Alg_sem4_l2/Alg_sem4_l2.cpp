e69f9c8ab3a9099ff193517a0b54316f61249218
Dimokku <32371247+Dimokku@users.noreply.github.com>
2019-04-02 04:08:53 +0300

    Update Alg_sem4_l2.cpp

--- Alg_sem4_l2/Alg_sem4_l2.cpp
+++ Alg_sem4_l2/Alg_sem4_l2.cpp
@@ -9,17 +9,6 @@ int main()
 {
 	cout << "Laboratory work #2 \"Associative array\"\nKarmanov D. gr.7302 var. 2\n";
 	Map<string, double> tree;
-	/*tree.insert("January", 15);
-	tree.insert("February", 6);
-	tree.insert("March", 9);
-	tree.insert("April", 5);
-	tree.insert("May", 8);
-	tree.insert("June", 15);
-	tree.insert("Jule", 1);
-	tree.insert("Cucumber", 133);
-	tree.print(tree.root, 0);
-	tree.remove("May");
-	tree.print(tree.root, 0);*/
 
 	tree.insert("January", 15);
 	tree.insert("February", 6);
@@ -27,34 +16,13 @@ int main()
 	tree.insert("April", 5);
 	tree.insert("May", 8);
 	tree.insert("June", 15);
+	tree.print(); cout << "\n\n\n";
 	tree.insert("Jule", 1);
-	tree.print(tree.root, 0);
-	tree.remove("Jule"); //it is red node*/
-	tree.remove("June");
-	cout << *tree.get_keys() << endl;
-	/*tree.insert("January", 15);
-	tree.insert("February", 6);
-	tree.insert("March", 9);
-	tree.insert("April", 5);  // BUT HERE ALL RIGHT
-	cout << "ROOT IS " << tree.root->key;*/
-	tree.print(tree.root, 0);
-
-	/*cout << "\nNOW IT IS\n";
-	string *a2 = new string[6];
-	auto *iter = tree.create_dft_iterator();
-	int i = 0;
-	while (iter->has_next()) {
-		string kek = iter->next()->key;
-		a2[i] = kek;
-		//cout << a2[i] << " ";
-		i++;
-	}
-
-	for (int i = 0; i < 6; i++)
-		cout << a2[i] << " "*/
-
+	tree.insert("Ma", 23);
+	tree.print(); cout << "\n\n\n";
+	//tree.remove("May");
 	tree.clear();
-	//tree.print(tree.root, 0);
+	tree.print();
 
 	cout << "\nBon Voyage!\n";
 	return 0;
